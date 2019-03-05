af589006c89e6eb9e8ec66d611eaf5bee37615eb
artembakhanov <artembakhanov@gmail.com>
2019-03-05 23:02:10 +0300

    task 2 2

--- Assignments/2/src/Main.java
+++ Assignments/2/src/Main.java
@@ -1,15 +1,16 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        Solutions.task2_2();
+        Solutions.task2_3();
     }
 }
 
 /**
  * This class is used for calculating difference between two strings.
- * This distance is called Damerau–Levenshtein distance because we are allowed to swap adjacent chars.
+ * This distance is called Damerau–Levenshtein distance (Optimal string alignment distance) because we are allowed to swap adjacent chars.
  * This is used for solution for all subtasks of task 2 from the assignment.
  * @see <a href="https://en.wikipedia.org/wiki/Damerau%E2%80%93Levenshtein_distance">"Damerau–Levenshtein distance" on Wikipedia</a>
  *
@@ -48,9 +49,10 @@ class DamerauLevenshteinDist {
                 if (a.charAt(i - 1) == b.charAt(j - 1)) D[i][j] = D[i - 1][j - 1]; // 0 - replace cost (replace not needed here)
                 else D[i][j] = D[i - 1][j - 1] + 1; // 1 - replace cost
 
-                D[i][j] = min(D[i][j], D[i - 1][j] + 1, D[i][j - 1] + 1); // replace, delete and insert cost
+                D[i][j] = min(D[i][j], D[i - 1][j] + 1, D[i][j - 1] + 1); // 1, 1, 1 - replace, delete and insert cost
 
-                if (i > 1 && j > 1 && a.charAt(i - 1) == b.charAt(j - 2) && a.charAt(i - 2) == b.charAt(i - 1))
+                // check for swap
+                if (i > 1 && j > 1 && a.charAt(i - 1) == b.charAt(j - 2) && a.charAt(i - 2) == b.charAt(j - 1))
                     D[i][j] = min(D[i][j], D[i - 2][j - 2] + 1);
             }
         }
@@ -89,6 +91,7 @@ class Solutions {
 
     /**
      * Solution for task 2.1 (Misspellings)
+     * Codeforces submission number: 50853226
      */
     public static void task2_1() {
         Scanner scanner = new Scanner(System.in);
@@ -104,6 +107,7 @@ class Solutions {
 
     /**
      * Solution for task 2.2 (Correction suggestions)
+     * Codeforces submission number: 50853832
      */
     public static void task2_2() {
         Scanner scanner = new Scanner(System.in);
@@ -135,12 +139,64 @@ class Solutions {
 
         // print all suggestions
         for (int i = 0; i < sugNum; i++) {
-            System.out.println(dict[suggestions[i]] + " ");
+            System.out.print(dict[suggestions[i]] + (i != sugNum - 1 ? " " : ""));
         }
     }
 
+    /**
+     * Solution for task 2.3 (Text autocorrection)
+     * Codeforces submission number: 50864343
+     */
     public static void task2_3() {
+        Scanner scanner = new Scanner(System.in);
 
+        String keys[] = scanner.nextLine().split("[^a-z]+"); // split by any non word element. word is [a-z]+
+        Map<String, Integer> wordsCount = new HashMap<>();
+
+        // count all words in a hashmap
+        // key - a word, value - number of repetitions
+        for (String key: keys) {
+            wordsCount.putIfAbsent(key, 0);
+            wordsCount.put(key, wordsCount.get(key) + 1);
+        }
+
+        // sorted map of words (by frequency)
+        // Map.Entry<String, Integer>wordsCount.entrySet().toArray();
+        List<Map.Entry<String, Integer>> dict = new ArrayList<>(wordsCount.entrySet());
+        dict.sort(Collections.reverseOrder(Map.Entry.comparingByValue())); // sort by value
+
+        // read next line
+        String text = scanner.nextLine();
+
+        // divide the text to substrings.
+        // substring is either [a-z]+ (words) or [^a-z]+ (punctuation or other symbols)
+        ArrayList<String> words = new ArrayList<>();
+        Matcher m = Pattern.compile("([a-z]+)|([^a-z]+)").matcher(text);
+        while (m.find()) {
+            words.add(m.group()); // find all matches
+        }
+
+        for (int i = 0; i < words.size(); i++) {
+            // if it is not a word
+            if (!words.get(i).matches("[a-z]+")) continue;
+
+            int min = 100000, minIndex = -1;
+            for (int j = 0; j < dict.size(); j++) {
+                int curDist;
+
+                // if we found word with smaller distance
+                if ((curDist = DamerauLevenshteinDist.estimate(words.get(i), dict.get(j).getKey())) < min) {
+                    min = curDist;
+                    minIndex = j;
+                }
+            }
+            words.set(i, dict.get(minIndex).getKey()); // replace i-th word from text with suggested word
+
+        }
+
+        // print text (with saved punctuation)
+        for (String word: words) {
+            System.out.print(word);
+        }
     }
-
 }
